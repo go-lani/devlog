07ecc82d75c4f78dbaf4842479107c08f701b6f7
이철환 <go.lani.developer@gmail.com>
2023-08-23 18:28:28 +0900

    feat: code 경로 및 언어 표시 UI  추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import React, { Suspense } from 'react';
 import { getPost } from '@/service/posts';
 import MarkdownViewer from '@/components/post/MarkdownViewer';
 
@@ -10,8 +9,26 @@ export default async function DetailPage({ params: { slug } }: IProps) {
   const post = await getPost(slug);
   return (
     <>
-      <h1>{post.meta.title}</h1>
-      <MarkdownViewer content={post.content} />
+      <div className="container-layout border-style grow-0 border-b">
+        <div className="content-layout border-style border-x">
+          <h1>{post.meta.title}</h1>
+          <h2>{post.meta.description}</h2>
+          {post.meta.series && (
+            <p>
+              {post.meta.series}{' '}
+              {post.meta.series_number && `- ${post.meta.series_number}`}
+            </p>
+          )}
+          <ul>
+            <li />
+          </ul>
+        </div>
+      </div>
+      <div className="container-layout border-style">
+        <div className="content-layout border-style border-x p-5">
+          <MarkdownViewer content={post.content} />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/post/SyntaxHighlighter.tsx
+++ src/components/post/SyntaxHighlighter.tsx
@@ -8,14 +8,23 @@ type SyntaxProps = {
   language: string;
 } & CodeProps;
 
-export default function Syntax({ children, language, ...props }: SyntaxProps) {
+export default function Syntax({
+  children,
+  language,
+  node,
+  ...props
+}: SyntaxProps) {
   return (
-    <SyntaxHighlighter
-      children={String(children).replace(/\n$/, '')}
-      {...props}
-      style={oneDark}
-      language={language}
-      PreTag="div"
-    />
+    <>
+      <span className="language-text">{language}</span>
+      <SyntaxHighlighter
+        children={String(children).replace(/\n$/, '')}
+        {...props}
+        style={oneDark}
+        language={language}
+        showLineNumbers
+        customStyle={{ margin: 0, borderRadius: 0 }}
+      />
+    </>
   );
 }
