2be4fac84152cc91a7cb7035b1aaa1e4f0e31bdf
이철환 <go.lani.developer@gmail.com>
2023-08-29 14:30:59 +0900

    refactor: 변수명 오타 수정

--- src/components/post/TableOfContent.tsx
+++ src/components/post/TableOfContent.tsx
@@ -13,15 +13,15 @@ export default function TableOfContent({ toc }: Props) {
     <div className="mx-[-1rem] p-4 text-sm">
       <p className="font-bold text-white">table of contents</p>
       <ul className="mt-2 flex max-h-[400px] flex-col gap-2 overflow-y-auto overflow-x-hidden text-neutral-500">
-        {toc.map((t) => (
-          <li key={`${t.level}-${t.title}`}>
-            <Link href={`${pathname}${t.href}`} replace>
-              {t.title}
+        {toc.map((depth1) => (
+          <li key={`${depth1.level}-${depth1.title}`}>
+            <Link href={`${pathname}${depth1.href}`} replace>
+              {depth1.title}
             </Link>
-            {t.children && (
+            {depth1.children && (
               <ul className="mt-2 flex flex-col gap-2 pl-4">
-                {t.children.map((depth2) => (
-                  <li>
+                {depth1.children.map((depth2) => (
+                  <li key={`${depth2.level}-${depth2.title}`}>
                     <Link href={`${pathname}${depth2.href}`} replace>
                       {depth2.title}
                     </Link>
--- src/components/post/Tags.tsx
+++ src/components/post/Tags.tsx
@@ -3,19 +3,19 @@ type Props = {
 };
 
 export default function Tags({ tags }: Props) {
-  const categories = [...tags];
-  const displayCategories = categories.splice(0, 3);
+  const copiedtags = [...tags];
+  const displaytags = copiedtags.splice(0, 3);
   return (
     <div className="border-style mx-[-1rem] border-t p-4 text-sm">
       <p className="font-bold text-white">tags</p>
       <ul className="mt-2 flex flex-wrap gap-2">
-        {displayCategories.map((category) => (
-          <li key={category} className="text-neutral-400">
-            {category}
+        {displaytags.map((tag) => (
+          <li key={tag} className="text-neutral-400">
+            {tag}
           </li>
         ))}
-        {categories.length > 0 && (
-          <li className="text-neutral-400">외 {categories.length}</li>
+        {copiedtags.length > 0 && (
+          <li className="text-neutral-400">외 {copiedtags.length}</li>
         )}
       </ul>
     </div>
