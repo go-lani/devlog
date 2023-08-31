cd6c4a2d57dadfae5c5c8212df397df3ec72032b
이철환 <go.lani.developer@gmail.com>
2023-08-31 10:55:43 +0900

    feat: metadata 추가

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -3,8 +3,8 @@ import { getAllPostSeries } from '@/service/posts';
 import ListPage from '@/components/series/list/ListPage';
 
 export const metadata: Metadata = {
-  title: 'devlog - series',
-  description: "lani's devlog",
+  title: '시리즈',
+  description: '라니의 시리즈 목록',
 };
 
 export default async function Series() {
