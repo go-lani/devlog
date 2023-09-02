d13b3685999db1e16e7560920875c87dd4b231c5
go-lani <go.lani.developer@gmail.com>
2023-09-02 12:22:24 +0900

    refactor: toc 링크 한글도 포함되도록 수정

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,16 +1,9 @@
 export const toKebabCase = (str: string): string => {
-  return (
-    str
-      // Convert the string to lowercase
-      .toLowerCase()
-      // Replace spaces, underscores, and camelCase with hyphen
-      .replace(/[\s_]+/g, '-')
-      .replace(/([a-z])([A-Z])/g, '$1-$2')
-      // Remove all non-word characters (everything except numbers, letters, and hyphens)
-      .replace(/[^a-z0-9-]+/g, '')
-      // Replace multiple consecutive hyphens with a single one
-      .replace(/--+/g, '-')
-      // Trim any starting or trailing hyphens
-      .replace(/^-+|-+$/g, '')
-  );
+  return str
+    .toLowerCase()
+    .replace(/[\s_]+/g, '-')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[^a-z0-9가-힣-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+|-+$/g, '');
 };
