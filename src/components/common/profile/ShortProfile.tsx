d269d7fb60b5145eadf096cd73b58f0e72f4f4a6
go-lani <go.lani.developer@gmail.com>
2023-09-03 10:22:20 +0900

    feat: 상세 페이지 generateStaticParams 추가

--- src/components/common/profile/ShortProfile.tsx
+++ src/components/common/profile/ShortProfile.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
-
 export default function ShortProfile() {
   return (
     <div className="border-style mx-[-1rem] flex items-center border-b p-4">
       <div className="relative h-[40px] w-[40px] rounded-full border border-transparent bg-white">
-        <Image src="/assets/images/character.webp" alt="lani" fill />
+        <img src="/assets/images/character.webp" alt="lani" />
       </div>
       <p className="ml-4 text-app-white">developer Lani</p>
     </div>
