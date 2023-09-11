ac6d353405cc4827a852c28313a0b37aa6a94636
이철환 <go.lani.developer@gmail.com>
2023-09-11 17:16:20 +0900

    style: image viewer width 스타일 수정

--- src/components/common/imgViewer/ImgViewer.tsx
+++ src/components/common/imgViewer/ImgViewer.tsx
@@ -24,11 +24,11 @@ export default function ImgViewer({
     >
       <OuterHandler onOutsideClick={onCloseImgViewer}>
         <>
-          <div className="absolute left-[50%] top-[50%] w-full translate-x-[-50%] translate-y-[-50%] lg:w-[742px]">
+          <div className="absolute left-[50%] top-[50%] flex w-full translate-x-[-50%] translate-y-[-50%] justify-center lg:w-[742px]">
             <img
               src={images[activeIndex].src}
               alt=""
-              className=" max-h-[60vh] w-full"
+              className="max-h-[60vh] max-w-full"
             />
           </div>
           <div className="absolute bottom-0 mt-auto flex h-[20vh] w-full flex-col items-center justify-center py-2">
