a558067366d0e4a195ae088e9cb14461af5969ff
이철환 <go.lani.developer@gmail.com>
2023-10-26 11:09:21 +0900

    fix: 오탈자 수정

--- src/components/playground/Ground.tsx
+++ src/components/playground/Ground.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { Modal } from '@lani.ground/react-modal';
 import { OutsideClickHandler } from '@lani.ground/react-outside-click-handler';
+import '@lani.ground/react-modal/css';
 // import { OutsideClickHandler } from '@lani.ground/react-outside-click-handler';
 import { useState } from 'react';
 
@@ -14,6 +16,31 @@ export default function Ground() {
     <>
       <main className="flex flex-col text-white">
         <h1 className="p-20 text-2xl">playground</h1>
+
+        <Modal
+          trigger={<button type="button">hihi</button>}
+          component={(closeModal) => (
+            <div style={{ color: '#fff', background: 'blue' }}>
+              <button type="button" onClick={closeModal}>
+                X
+              </button>
+              <Modal
+                name="modal2"
+                trigger={<button type="button">모달 2</button>}
+                component={(closeModal) => (
+                  <div style={{ background: 'red' }}>
+                    <button type="button" onClick={closeModal}>
+                      XX
+                    </button>
+                    <p>모달2</p>
+                  </div>
+                )}
+                dim="rgba(0,0,0,.8)"
+              />
+              <p>모달</p>
+            </div>
+          )}
+        />
 
         <div className="relative">
           <button type="button" onClick={() => setIsOpen((prev) => !prev)}>
