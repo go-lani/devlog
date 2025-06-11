07a63d3337e60b27b7e0c21ac5e482e1df9ea2bb
이철환 <go.lani.developer@gmail.com>
2025-06-11 14:16:24 +0900

    fix: build 에러

--- src/components/base/Navigation.tsx
+++ src/components/base/Navigation.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Modal } from '@lani.ground/react-modal';
+import localFont from 'next/font/local';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
@@ -9,8 +10,6 @@ import { MENU } from '@/constants/app';
 
 import '@lani.ground/react-modal/css';
 import MobileNavigation from './MobileNavigation';
-
-import localFont from 'next/font/local';
 
 const shareTechMono = localFont({
   src: [
